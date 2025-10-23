63fb53ce3e01e9286628c27753b7e248c5c728ee
Gean Saturno <geangoncalves.gst@ciclic.com.br>
2025-10-23 11:34:25 -0300

    chore: styles

--- src/components/TodoInput/TodoInput.tsx
+++ src/components/TodoInput/TodoInput.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import type { TodoInputProps } from "./TodoInput.types";
+import style from './TodoInput.module.scss'
 
 export function TodoInput({onAdd} : TodoInputProps) {
 
@@ -12,8 +13,9 @@ export function TodoInput({onAdd} : TodoInputProps) {
     }
 
     return (
-        <div>
+        <div className={style.todo_input}>
             <input type="text" value={input} onChange={e => setInput(e.target.value)} onKeyDown={e => /enter/i.test(e.code) && sendInput()}/>
+            <button className={style.todo_input__button} onClick={sendInput}>Salvar</button>
         </div>
     )
 }
